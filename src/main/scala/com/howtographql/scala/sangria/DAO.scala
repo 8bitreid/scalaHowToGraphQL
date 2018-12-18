ce141f3713a8496a2bc3836b579bb95cd59d2aa2
Reid Mewborne <rmewborne@me.com>
2018-12-17 22:31:33 -0600

    Handling Arguments

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -1,9 +1,19 @@
 package com.howtographql.scala.sangria
+
 import DBSchema._
 import slick.jdbc.H2Profile.api._
 
 import scala.concurrent.Future
+import com.howtographql.scala.sangria.models.Link
 
 class DAO(db: Database) {
   def allLinks: Future[Seq[models.Link]] = db.run(Links.result)
+
+  def getLink(id: Int): Future[Option[Link]] = db.run(
+    Links.filter(_.id === id).result.headOption
+  )
+
+  def getLinks(ids: Seq[Int]): Future[Seq[Link]] = db.run(
+    Links.filter(_.id inSet ids).result
+  )
 }
--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -8,12 +8,24 @@ import sangria.macros.derive._
 
 object GraphQLSchema {
   implicit val LinkType: ObjectType[Unit, Link] = deriveObjectType[Unit, Link]()
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
 
   // 2
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link",
+        OptionType(LinkType),
+        arguments = Id :: Nil, // it's a list!
+        resolve = c => c.ctx.dao.getLink(c.arg(Id))
+      ),
+      Field("links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg(Ids))
+      )
     )
   )
 
