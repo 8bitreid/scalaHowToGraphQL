3bf937f4331e9095a419d08c558e585966d7d79b
Reid Mewborne <rmewborne@me.com>
2018-12-20 21:11:59 -0600

    Custom Scalars

--- src/main/scala/com/howtographql/scala/sangria/models/package.scala
+++ src/main/scala/com/howtographql/scala/sangria/models/package.scala
@@ -1,6 +1,9 @@
 package com.howtographql.scala.sangria
 
 import sangria.execution.deferred.HasId
+import akka.http.scaladsl.model.DateTime
+import sangria.validation.Violation
+
 
 package object models {
   trait Identifiable {
@@ -9,5 +12,9 @@ package object models {
   object Identifiable {
     implicit def hasId[T <: Identifiable]: HasId[T, Int] = HasId(_.id)
   }
-  case class Link(id: Int, url: String, description: String) extends Identifiable
+  case class Link(id: Int, url: String, description: String, createdAt: DateTime) extends Identifiable
+
+  case object DateTimeCoerceViolation extends Violation {
+    override def errorMessage: String = "Error during parsing DateTime"
+  }
 }
